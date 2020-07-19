1503567b573245b036cd9969fe0841b3c080a3ea
4fterlunch <mholmes2@student.unimelb.edu.au>
2020-07-20 07:28:50 +1000

    change to mixin

--- Composite.js
+++ Composite.js
@@ -1,51 +1,45 @@
 /**
  * @author Michael Holmes / https://github.com/4fterlunch
  * @version 1.0.0
- * @description Useful composite object for creating composite pattern
- *              objects.
+ * @description Useful composite mixin object for adding composite pattern
+ *              functionality to a js object
  */
 
 var UID = 0;
-
 /**
- * Composite object
- * @param {{}} optional props param
+ * @description A composite mixin component
+ * @example basic usage: var a = CompositeMixin("myComposite")(myObject);
+ * @example advanced usage: use a factory function for composition
+ * @param {string} optional name
  */
-function Composite(props) {
-  this.props = props || {};
-  this.uid = UID++;
-  this.name = this.props.name || "";
-  this.children = [];
-  this.parent = null;
-}
+const CompositeMixin = ({ name = "" } = {}) => (obj) => {
+  let uid = UID++;
+  let children = [];
+  let parent = null;
 
-Object.assign(Composite.prototype, {
-  update() {
-    return this;
-  },
-  add(child) {
-    child.parent = this;
-    this.children.push(child);
-    return this;
-  },
-  remove(child) {
-    this.children.splice(this.children.indexOf(child));
-    return this;
-  },
-  getChildByName(name) {
-    return this.children.find(function(child) {
-      return child.name === name;
-    })
-  }
-});
-
-/**
- * @description Functional composition method to add Composite to any object
- * @param {*} obj 
- */
-function addComposite(obj) {
-  Object.assign(obj, new Composite);
-  obj.prototype = Object.assign(Composite.prototype);
-}
-
-export {Composite, addComposite}
+  return {
+    ...obj,
+    name,
+    uid: uid,
+    children: children,
+    parent: parent,
+    update() {
+      for (var i = 0; i < this.children.length; i++) this.children[i].update();
+      return this;
+    },
+    add(child) {
+      child.parent = this;
+      this.children.push(child);
+      return this;
+    },
+    remove(child) {
+      this.children.splice(this.children.indexOf(child));
+      return this;
+    },
+    getChildByName(name) {
+      return this.children.find(function (child) {
+        return child.name === name;
+      });
+    },
+  };
+};
